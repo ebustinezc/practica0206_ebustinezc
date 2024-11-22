2c493c97b5721dec86275c80dc4dfb5ec9dd5b97
ebustinezc <ebustinezc@educacion.navarra.es>
2024-11-22 09:07:40 +0100

    Ejercicio1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,3 +2,10 @@
 # el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, 
 # pregunte al usuario por una divisa y muestre su símbolo 
 # o un mensaje de aviso si la divisa no está en el diccionario.
+
+Moneda = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+Usuario = input("Introduce una moneda ")
+if Usuario.title() in Moneda:
+    print(Moneda[Usuario.title()])
+else:
+    print("La moneda no esta")

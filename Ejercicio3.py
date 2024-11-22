1164a936405fc5a3c6b7afc5b2fdee76c6aea287
ebustinezc <ebustinezc@educacion.navarra.es>
2024-11-22 09:02:45 +0100

    escribir enunciados

---
+++ Ejercicio3.py
@@ -0,0 +1,11 @@
+# Escribir un programa que guarde en un diccionario los precios de los artículos de la tabla, 
+# pregunte al usuario por un artículo, un número de unidades y muestre por pantalla el precio 
+# de esa cantidad de producto. Si el producto no está en el diccionario debe mostrar un mensaje 
+# informando de ello.
+
+# Producto  Precio
+# Pan       1.40
+# Huevos    2.30
+# Cebolla   0.85
+# Aceite    4.35
+
